9fc006576ef0132ca4eaad1477dbc69eb2a3ecce
Zonylaw <sunny_law@hotmail.com>
2023-03-24 11:01:25 +0000

    add gitignore and env file

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import os
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exist("env.py"):
+    import env
 
 
 app = Flask(__name__)  # so python knows where to find template
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")  # root directory
@@ -31,8 +34,11 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
